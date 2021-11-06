906e00a70e4c1d3b8bcb2d1bb790cb241a68485d
MishaZ <mixa.zolotyonkov@gmail.com>
2021-11-06 18:15:55 +0300

    My First commit

---
+++ lab3/ClassPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class Point
+    {
+        public int a;
+        public int b;
+        public char sym;
+        public void Draw()
+        {
+            Console.SetCursorPosition(a,b);
+            Console.Write(sym);
+        }
+    
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point P = new Point();
+            P.a = 5;
+            P.b = 7;
+            P.sym = 'v';
+            P.Draw();
+        }
+    }
+}
